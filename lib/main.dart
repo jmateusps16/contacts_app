bba3fc42df500cd1c70392bf0cb3d921566280f8
Mateus Palmeira <jmateusps16@gmail.com>
2022-11-11 07:28:20 -0300

    Usando MaterialApp e removendo banner

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,10 @@ import 'package:contacts_app/views/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
